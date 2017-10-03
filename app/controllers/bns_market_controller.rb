58ee1e4eb703d89d544fed51a86b84b3a1cae495
wonderer <wonderv@gmail.com>
2017-10-03 15:58:00 +0900

    삼성 금강석 주머니 추가

--- app/controllers/bns_market_controller.rb
+++ app/controllers/bns_market_controller.rb
@@ -1,20 +1,52 @@
 class BnsMarketController < ApplicationController
   def main
 
-    item_list = [{ name: '월석', num: 60 },
-                 { name: '영석', num: 230 },
-                 { name: '영단', num: 230 },
-                 { name: '선단', num: 60 }]
-
-    @price = item_list.inject(0) do |sum, item|
-      sum += (BnsMarket.item_price(item[:name]) * item[:num])
+    made_item_infos = [
+        {
+            made_item_name: '진화석',
+            made_item_num: 35,
+            made_price: 180,
+            materials: [{ name: '월석', num: 60 },
+                        { name: '영석', num: 230 },
+                        { name: '영단', num: 230 },
+                        { name: '선단', num: 60 }]
+        },
+        {
+            made_item_name: '삼성 금강석 주머니',
+            made_item_num: 2,
+            made_price: 900,
+            materials: [{ name: '월석', num: 370 },
+                        { name: '영석', num: 850 },
+                        { name: '영단', num: 850 },
+                        { name: '선단', num: 370 }]
+        }
+    ]
+    @result = []
+    made_item_infos.each do |info|
+      @result << item_cost(info)
     end
 
-    @production_cost = 180
-    @total_cost = @price + @production_cost
+  end
+
+  def item_cost(made_item_info)
+    materials = made_item_info[:materials]
+
+    result = {}
+    result[:made_item_name] = made_item_info[:made_item_name]
+    result[:made_item_num] = made_item_info[:made_item_num]
+    result[:price] = materials.inject(0) do |sum, item|
+      sum += (BnsMarket.item_price(item[:name]) * item[:num])
+      puts "#{item[:name]}: #{BnsMarket.item_price(item[:name])}"
+      sum
+    end
+
+    result[:production_cost] = made_item_info[:made_price]
+    result[:total_cost] = result[:price] + result[:production_cost]
     # 가격 * 생산수 - 수수료(0.7%)
-    @item_price = BnsMarket.item_price('진화석') * 35 * 0.93
-    @earning_price = @item_price - @total_cost
+    result[:item_price] = BnsMarket.item_price(made_item_info[:made_item_name]) * made_item_info[:made_item_num] * 0.93
+    result[:earning_price] = result[:item_price] - result[:total_cost]
+
+    result
   end
 
   def main2
--- app/models/bns_market.rb
+++ app/models/bns_market.rb
@@ -6,10 +6,12 @@ class BnsMarket
       doc = Nokogiri::HTML(open("http://m.bns.plaync.com/bs/market/search?ct=&level=&stepper=&exact=1&sort=&type=&grade=&prevq=&q=#{URI.encode(itemName)}"))
       list = doc.xpath('//*[@id="listMarket"]/tbody/tr[*]/th')
       result = list.map do |e|
-        num = e.xpath('div[1]/span[2]')[0].children[0].to_s.strip
-        gold = e.xpath('div[2]/div[1]').css('.gold').children[0].to_s.strip
-        silver = e.xpath('div[2]/div[1]').css('.silver').children[0].to_s.strip
-        bronze = e.xpath('div[2]/div[1]').css('.bronze').children[0].to_s.strip
+        num = e.xpath('div[1]/span[2]')[0].children[0].to_s.gsub(/[$,\s]/,'').to_f
+        num = 1 if num == 0
+
+        gold = e.xpath('div[2]/div[1]').css('.gold').children[0].to_s.gsub(/[$,\s]/,'')
+        silver = e.xpath('div[2]/div[1]').css('.silver').children[0].to_s.gsub(/[$,\s]/,'')
+        bronze = e.xpath('div[2]/div[1]').css('.bronze').children[0].to_s.gsub(/[$,\s]/,'')
         price = "#{gold}.#{silver}#{bronze}"
 
         { num: num,
