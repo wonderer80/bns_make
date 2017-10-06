b817baf791687026c059fe10b1c7606613d1a736
wonderer <wonderv@gmail.com>
2017-10-06 12:18:35 +0900

    일미문 제작 상품 등 몇 종 추가

--- app/controllers/bns_market_controller.rb
+++ app/controllers/bns_market_controller.rb
@@ -6,33 +6,126 @@ class BnsMarketController < ApplicationController
             made_item_name: '진화석',
             made_item_num: 35,
             made_price: 180,
-            materials: [{ name: '월석', num: 60 },
+            made_time: 1.day,
+            materials: [
                         { name: '영석', num: 230 },
+                        { name: '월석', num: 60 },
                         { name: '영단', num: 230 },
-                        { name: '선단', num: 60 }]
+                        { name: '선단', num: 60 }
+            ]
+        },
+        {
+            made_item_name: '설혼석',
+            made_item_num: 20,
+            made_price: 900,
+            made_time: 5.days,
+            materials: [
+                        { name: '영석', num: 850 },
+                        { name: '월석', num: 370 },
+                        { name: '영단', num: 850 },
+                        { name: '선단', num: 370 }
+            ]
+        },
+        {
+            made_item_name: '명인 합성목편 묶음',
+            made_item_num: 6,
+            made_price: 540,
+            made_time: 3.days,
+            materials: [
+                { name: '영석', num: 695 },
+                { name: '월석', num: 175 },
+                { name: '영단', num: 695 },
+                { name: '선단', num: 175 }
+            ]
+        },
+        {
+            made_item_name: '진마골',
+            made_item_num: 9,
+            made_price: 90,
+            made_time: 12.hours,
+            materials: [
+                { name: '영석', num: 115 },
+                { name: '월석', num: 30 },
+                { name: '영단', num: 115 },
+                { name: '선단', num: 30 }
+            ]
         },
         {
             made_item_name: '삼성 금강석 주머니',
             made_item_num: 2,
             made_price: 900,
-            materials: [{ name: '월석', num: 370 },
+            made_time: 5.days,
+            materials: [
                         { name: '영석', num: 850 },
+                        { name: '월석', num: 370 },
                         { name: '영단', num: 850 },
-                        { name: '선단', num: 370 }]
+                        { name: '선단', num: 370 }
+            ]
         },
         {
             made_item_name: '팔각 금강석 주머니',
             made_item_num: 3,
             made_price: 900,
-            materials: [{ name: '월석', num: 370 },
+            made_time: 5.days,
+            materials: [
                         { name: '영석', num: 850 },
+                        { name: '월석', num: 370 },
                         { name: '영단', num: 850 },
-                        { name: '선단', num: 370 }]
+                        { name: '선단', num: 370 }
+            ]
+        },
+        {
+            made_item_name: '순한 만두 음식상자',
+            made_item_num: 20,
+            made_price: 180,
+            made_time: 1.day,
+            materials: [
+                { name: '영석', num: 230 },
+                { name: '월석', num: 60 },
+                { name: '영단', num: 230 },
+                { name: '선단', num: 60 }
+            ]
+        },
+        {
+            made_item_name: '매운 만두 음식상자',
+            made_item_num: 12,
+            made_price: 180,
+            made_time: 1.day,
+            materials: [
+                { name: '영석', num: 230 },
+                { name: '월석', num: 60 },
+                { name: '영단', num: 230 },
+                { name: '선단', num: 60 }
+            ]
+        },
+        {
+            made_item_name: '용봉탕 음식상자',
+            made_item_num: 10,
+            made_price: 180,
+            made_time: 1.day,
+            materials: [
+                { name: '영석', num: 230 },
+                { name: '월석', num: 60 },
+                { name: '영단', num: 230 },
+                { name: '선단', num: 60 }
+            ]
+        },
+        {
+            made_item_name: '천년초 비약 상자',
+            made_item_num: 25,
+            made_price: 180,
+            made_time: 1.day,
+            materials: [
+                { name: '영석', num: 230 },
+                { name: '월석', num: 60 },
+                { name: '영단', num: 230 },
+                { name: '선단', num: 60 }
+            ]
         }
     ]
     @result = []
     made_item_infos.each do |info|
-      @result << item_cost(info)
+      @result << info.merge(item_cost(info))
     end
 
   end
@@ -41,8 +134,6 @@ class BnsMarketController < ApplicationController
     materials = made_item_info[:materials]
 
     result = {}
-    result[:made_item_name] = made_item_info[:made_item_name]
-    result[:made_item_num] = made_item_info[:made_item_num]
     result[:price] = materials.inject(0) do |sum, item|
       sum += (BnsMarket.item_price(item[:name]) * item[:num])
       puts "#{item[:name]}: #{BnsMarket.item_price(item[:name])}"
@@ -56,40 +147,5 @@ class BnsMarketController < ApplicationController
     result[:earning_price] = result[:item_price] - result[:total_cost]
 
     result
-  end
-
-  def main2
-# http://m.bns.plaync.com/bs/market/search?ct=&level=&stepper=&exact=1&sort=&type=&grade=&prevq=&q=월석
-    result = Wombat.crawl do
-      base_url "http://m.bns.plaync.com"
-      path "/bs/market/search?ct=&level=&stepper=&exact=1&sort=&type=&grade=&prevq=&q=월석"
-
-      headline 'xpath=//*[@id="listMarket"]/tbody'
-
-      explore 'xpath=//*[@id="listMarket"]/tbody/tr' do |e|
-        e
-      end
-      # //*[@id="listMarket"]/tbody/tr[1]/th/div[2]/div[1]/span[1]
-      what_list('xpath=//*[@id="listMarket"]/tbody/tr[*]/th/div[2]/div[1]', :list)
-
-      price_gold css: '#listMarket > tbody > tr:nth-child(1) > th > div.price > div.unit > span.gold'
-      price_silver css: '#listMarket > tbody > tr:nth-child(1) > th > div.price > div.unit > span.silver'
-      price_bronze css: '#listMarket > tbody > tr:nth-child(1) > th > div.price > div.unit > span.bronze'
-      #
-      # what_is "css=.column.secondary p", :html
-      #
-      # explore "xpath=//ul/li[2]/a" do |e|
-      #   e.gsub(/Explore/, "LOVE")
-      # end
-      #
-      # benefits do
-      #   first_benefit "css=.column.leftmost h3"
-      #   second_benefir "css=.column.leftmid h3"
-      #   third_benefit "css=.column.rightmid h3"
-      #   fourth_benefit "css=.column.rightmost h3"
-      # end
-    end
-    p result
-    render json: result.to_json
   end
 end
