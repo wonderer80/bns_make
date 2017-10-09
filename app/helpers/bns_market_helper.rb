3b9528ab43049210d5e8cbf92742c9f22e0d2bcd
wonderer <wonderv@gmail.com>
2017-10-09 17:27:44 +0900

    제작 소요 시간 및 시간당 이익 표기

--- app/helpers/bns_market_helper.rb
+++ app/helpers/bns_market_helper.rb
@@ -1,2 +1,13 @@
 module BnsMarketHelper
+  def convert_humun_time(second)
+    day, hour = (second/60/60).divmod(24)
+    time_str = ''
+    time_str = "#{day}D " if day > 0
+    time_str = time_str + "#{hour}H" if hour > 0
+    time_str
+  end
+
+  def earning_by_hour(earning_price, second)
+    earning_price / (second/60/60)
+  end
 end
